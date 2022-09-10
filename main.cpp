739e62bace6953069d45b2508ce5ad84a318ab08
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-10 12:50:16 +0600

    inline function

--- main.cpp
+++ main.cpp
@@ -4,22 +4,9 @@
 #include <cmath>
 using namespace std; 
 
-//here is int &a and int &b holds the address of a and b respectively and not the value of a and b in the swap function and 
-//hence the value of a and b in the main function is also changed
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
 
-void call_by_value(int a, int b) {
-    swap(a, b);
-    cout << "a = " << a << " b = " << b << endl;
-}
-
-void call_by_reference(int &a, int &b) {
-    swap(a, b);
-    cout << "a = " << a << " b = " << b << endl;
+inline int sum(int x, int y) {
+    return x + y;
 }
 
 int main()
@@ -30,13 +17,7 @@ int main()
     cout << "Enter the number y : ";
     cin >> y;
 
-
-    cout << "Call by value : " << endl;
-    call_by_value(x, y);
-    cout << "x = " << x << " y = " << y << endl;
-    cout << "Call by reference : " << endl;
-    call_by_reference(x, y);
-    cout << "x = " << x << " y = " << y << endl;
+    cout << "The sum of x and y is : " << sum(x, y) << endl;
 
 
     
