39661d62bbd26e30df53e26ff1a70aa132aa5e59
JubaerHossain <jubaer01.cse@gmail.com>
2022-10-08 21:55:25 +0600

    prime number

--- main.cpp
+++ main.cpp
@@ -1,46 +1,42 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define ld long double
-
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define maxe *max_element
+#define mine *min_element
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define point(a) fixed<<setprecision(a)
+#define uniq(a) sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
+#define tc int test; cin>>test; while(test--)
 using namespace std; 
+
+bool prime_number(ll n)
+{
+    if(n==1) return false;
+    if(n==2) return true;
+    if(n%2==0) return false;
+    for(ll i=3; i*i<=n; i+=2)
+    {
+        if(n%i==0) return false;
+    }
+    return true;
+}
 
 int main()
 {
+    ll n;
+    cin>>n;
 
+    if(prime_number(n)) cout<<"Prime"<<endl;
+    else cout<<"Not Prime"<<endl;
 
-
-    ll a = 12;
-    ll b = 25;
-
-    // and operator
-    cout << "And is : " << (a & b) << endl;
-
-    // or operator
-
-    cout << "Or is : " << (a | b) << endl;
-
-
-
-    // xor of all numbers from 1 to n
-    ll ans = a^b;
-    cout<<"xor ans = "<<ans<<endl;
-
-    // 2's complement
-
-    cout << "2's complement of - 150 is : " << (~(-150)) << endl;
-
-
-
-    // left shift
-    ll c = 4;
-    ll d = c << 2;
-    cout<<"left shift = "<<d<<endl;
-
-    // right shift
-
-    ll e = 16;
-    ll f = e >> 2;
-    cout<<"right shift = "<<f<<endl;
-
+    
+    
     return 0;
 }
