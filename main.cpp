2e3c5488183ee61e1992ac3149f813b3b98601ff
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-06 11:14:29 +0600

    palindrome

--- main.cpp
+++ main.cpp
@@ -4,27 +4,25 @@
 #include <cmath>
 using namespace std; 
 
+int palindrome(int n){
+    int temp = n;
+    int rev = 0;
+    while(temp > 0){
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+    return rev;
+}
+
 int main()
 {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
+    int n= 1215;
 
-    // palindrome series of n numbers in reverse order
-    for (int i = n; i >= 1; i--)
-    {
-        int num = i;
-        int rev = 0;
-        while (num > 0)
-        {
-            int rem = num % 10;
-            rev = rev * 10 + rem;
-            num = num / 10;
-        }
-        if (i == rev)
-        {
-            cout << i << " ";
-        }
+    if(palindrome(n) == n){
+        cout << "Palindrome" << endl;
+    }
+    else{
+        cout << "Not Palindrome" << endl;
     }
     
     
