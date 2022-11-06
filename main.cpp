bb90872431bd99cc2f573fc09c34e330a3b00fa2
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-06 15:40:44 +0600

    reverse

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 #define ld long double
 #define pb push_back
@@ -7,32 +7,42 @@
 #define ppf pop_front
 #define maxe *max_element
 #define mine *min_element
-#define mem(a,b) memset(a,b,sizeof(a))
-#define all(a) (a).begin(),(a).end()
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) ((a)*((b)/gcd(a,b)))
-#define point(a) fixed<<setprecision(a)
-#define uniq(a) sort(all(a)); (a).erase(unique((a).begin(),(a).end()),(a).end())
-#define tc int test; cin>>test; while(test--)
-using namespace std; 
-
-int reverse(int n){
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) (a).begin(), (a).end()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) * ((b) / gcd(a, b)))
+#define point(a) fixed << setprecision(a)
+#define uniq(a)   \
+    sort(all(a)); \
+    (a).erase(unique((a).begin(), (a).end()), (a).end())
+#define tc       \
+    int test;    \
+    cin >> test; \
+    while (test--)
+using namespace std;
+class Solution {
+public:
+    int reverse(int n) {
     int rev=0;
-    while(n>0){
-        rev=rev*10+n%10;
-        n/=10;
+        while(n!=0){
+            int pop=n%10;
+            n/=10;
+            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)) return 0;
+            if(rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8)) return 0;
+            rev=rev*10+pop;
+        }
+        return rev;
+        
     }
-    return rev;
-
-}
-
-
+};
 
 int main()
 {
     int n = 17683;
 
-    cout<<reverse(n);
+    Solution s;
+    cout << s.reverse(n) << endl;
+
 
     return 0;
-}   
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-floor
+ int rev=0;    
+    while(n>0){
+        n/=10;
+        if(rev>INT_MAX || rev<INT_MIN)
+            return 0;
+        rev=rev*10+n%10;
+        
+    }
+    return rev;
