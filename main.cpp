20ff9b6f4cf9c029f1be665f5e750c929183a6f9
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-06 13:14:44 +0600

    done

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-int GCD(int a, int b) {
-    if (b == 0) return a;
-    return GCD(b, a % b);
+int Armstrong(int a)
+{
+    int temp = a;
+    int count = to_string(a).length();
+    int sum = 0;
+
+    while (temp != 0)
+    {
+        int digit = temp % 10;
+        // cout << digit << endl;
+        cout << pow(digit, count) << endl;
+        cout << "old sum: " << sum << endl;
+        int power = pow(digit, count);
+        sum = sum + power;
+        cout << "sum is " << sum << endl;
+        temp /= 10;
+    }
+    cout << sum << endl;
+    if (sum == a)
+    {
+        cout << a << " is an Armstrong number." << endl;
+    }
+    else
+    {
+        cout << a << " is not an Armstrong number." << endl;
+    }
+    return 0;
 }
 
 int main()
 {
-    int x = 6, y = 3;
-    cout << GCD(x, y) << endl;   
-    
+    int x = 153;
+    Armstrong(x);
+
     return 0;
 }
