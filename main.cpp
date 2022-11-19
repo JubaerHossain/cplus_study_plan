06020655bcabc8ecb68b97d0de78e57946cdccca
JubaerHossain <jubaer01.cse@gmail.com>
2022-11-19 10:26:26 +0600

    peak value get from an array

--- main.cpp
+++ main.cpp
@@ -1,48 +1,27 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define ld long double
-#define pb push_back
-#define pf push_front
-#define ppb pop_back
-#define ppf pop_front
-#define maxe *max_element
-#define mine *min_element
-#define mem(a, b) memset(a, b, sizeof(a))
-#define all(a) (a).begin(), (a).end()
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) * ((b) / gcd(a, b)))
-#define point(a) fixed << setprecision(a)
-#define uniq(a)   \
-    sort(all(a)); \
-    (a).erase(unique((a).begin(), (a).end()), (a).end())
-#define tc       \
-    int test;    \
-    cin >> test; \
-    while (test--)
+#include<iostream>
+
 using namespace std;
-class Solution {
-public:
-    int reverse(int n) {
-    int rev=0;
-        while(n!=0){
-            int pop=n%10;
-            n/=10;
-            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)) return 0;
-            if(rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8)) return 0;
-            rev=rev*10+pop;
-        }
-        return rev;
-        
-    }
-};
 
-int main()
-{
-    int n = 17683;
+int peakEleBruteForce(int arr[], int n) {
+    cout << arr[0] << " " << arr[1] << endl;
+  if (arr[0] >= arr[1]){
+    return arr[0];
+  }
 
-    Solution s;
-    cout << s.reverse(n) << endl;
+  for (int i = 1; i < n - 1; i++){
+    if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+      return arr[i];
+  }
 
+  return arr[n - 1];
+}
 
-    return 0;
+int main() {
+
+  int arr[] = { 1, 3, 20, 4, 1, 0 };
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  cout << "Peak Element is " << peakEleBruteForce(arr, n);
+
+  return 0;
 }
